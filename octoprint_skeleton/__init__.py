6a9ac3c32bfc2b42dff526237f54733971c31fe9
jakpokcz <68732868+jakpokcz@users.noreply.github.com>
2020-07-24 13:45:09 +0200

    s

--- octoprint_skeleton/__init__.py
+++ octoprint_skeleton/__init__.py
@@ -3,11 +3,18 @@ from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin):
+class OctoWifiChanger(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Change Wifi")
+        self._logger.info("OctoWifi (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    def get_template_vars(self):
+        return dict(url=self._settings.get(["url"]))
+
 
 __plugin_name__ = "OctoWifiChanger"
 __plugin_pythoncompat__ = ">=2.7,<4"
-__plugin_implementation__ = HelloWorldPlugin()
+__plugin_implementation__ = OctoWifiChanger()
