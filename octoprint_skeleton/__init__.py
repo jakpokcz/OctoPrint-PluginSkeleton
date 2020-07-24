57e881f5b8fe8e9d6aa512023d94659833af4c9b
jakpokcz <68732868+jakpokcz@users.noreply.github.com>
2020-07-24 13:04:22 +0200

    Update __init__.py

--- octoprint_skeleton/__init__.py
+++ octoprint_skeleton/__init__.py
@@ -14,5 +14,5 @@ class SkeletonPlugin(octoprint.plugin.TemplatePlugin):
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Plugin Skeleton"
+__plugin_name__ = "OctoWifi Changer"
 __plugin_implementation__ = SkeletonPlugin()
