32f94af06fad506dea3471354b63f12e0c646186
jakpokcz <68732868+jakpokcz@users.noreply.github.com>
2020-07-24 13:01:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "OctoWifiChanger"
+plugin_identifier = "skeleton"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_%s" % plugin_identifier
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoWifiChanger"
+plugin_name = "OctoWifi Changer"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Change your Wifi settings"
+plugin_description = "Change your Wifi Settings"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Poky"
